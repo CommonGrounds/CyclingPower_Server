cf1e2288148e0642026524baec4b4a4392f1e6aa
ryzen <java4now@gmail.com>
2025-08-08 00:16:16 +0200

    MEGA fix

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+-- src/main/resources/schema.sql
+CREATE TABLE app_user (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
